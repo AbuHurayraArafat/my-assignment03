578db6151b2405ae3c8e919cd942e275a026e9a5
MD Abu Hurayra Arafat <arafatnice333@gmail.com>
2025-08-04 20:00:15 +0600

    third problem solved

--- assignment03/final.js
+++ assignment03/final.js
@@ -4,3 +4,27 @@ var halfArea = area / 2;
 console.log(area);
 console.log(halfArea);
 
+
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 10000;
+if (money >= 25000) {
+  console.log("Laptop");
+} else if (money >= 10000) {
+  console.log("Cycle");
+} else {
+  console.log("Chocolate");
+}
+
+
+
+
+
+/** Problem -03 ( Medicine Planner ) */
+var lastDay = 11;
+for (var day = 1; day <= lastDay; day++) {
+  if (day % 3 === 0) {
+    console.log(day + " - medicine");
+  } else {
+    console.log(day + " - rest");
+  }
+}
--- assignment03/problem2.js
+++ assignment03/problem2.js
@@ -1,5 +1,5 @@
 /** Problem -02 ( Cycle or Laptop ) */
-var money = 10000;
+var money = 25000;
 if (money >= 25000) {
   console.log("Laptop");
 } else if (money >= 10000) {
